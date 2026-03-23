a8b2fb7940a98b4d99df29275ffd41f0d830eade
DomecStoces <dominik.stoces@mendelu.cz>
2026-03-23 10:26:29 +0100

    revision to beta-diverzity of elevational gradient

--- Beta-diversity.R
+++ Beta-diversity.R
@@ -1,49 +1,39 @@
-# Number of species for each row # 
 library(readxl)
 library(writexl)
-df <- read_excel("Beskydy_2007_2008_traits_final.xlsx", sheet = "chilo_diplo_iso_compo_names")
-df$species_richness <- rowSums(df[, -1] > 0)
-write_xlsx(df[, c("ID", "species_richness")], "species_richness.xlsx")
-
 library(vegan)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
-library(readxl)
 
 # 1.
-df <- read_excel("Beskydy_2007_2008_traits_final.xlsx", sheet = "carabids_FD")
-compo_names <- read_excel("Beskydy_2007_2008_traits_final.xlsx", sheet = "carabids_compo_names")
+df <- read_excel("data_long.xlsx", sheet = "Sheet1")
+# 2. Aggregate and reshape from Long to Wide format
+# Group by Locality and your environmental variables, then sum abundances
+df_agg <- df %>%
+  group_by(Locality, Elevation, Exposition) %>%
+  # Sum abundances in case there are multiple entries for the same species at a locality
+  summarise(Abundance = sum(Abundance), .groups = "drop") %>%
+  # Pivot to wide format: Species become columns, Abundance becomes values
+  pivot_wider(names_from = Species, values_from = Abundance, values_fill = list(Abundance = 0))
 
-# 2. Initial
-metadata <- df
-metadata$ID <- 1:nrow(metadata)
-metadata_matched <- metadata[metadata$ID %in% compo_names$ID, ]
-
-# 3. Prepare raw community matrix
-comm_matrix <- as.matrix(compo_names[, -1])
-rownames(comm_matrix) <- compo_names$ID
-
-# 4. AGGREGATE DATA
-# Combine metadata and species, then sum species counts per locality
-df_combined <- bind_cols(metadata_matched, as.data.frame(comm_matrix))
-
-df_agg <- df_combined %>%
-  drop_na(Trees, Altitude) %>%
-  group_by(Locality, Trees, Altitude) %>%
-  summarise(across(all_of(colnames(comm_matrix)), sum), .groups = "drop") %>%
+# 3. Prepare Metadata & Scale Continuous Variables
+df_agg <- df_agg %>%
   mutate(
-    Altitude_scaled = scale(Altitude),
-    Trees = as.factor(Trees),
+    Elevation_scaled = scale(Elevation),
+    Exposition = as.factor(Exposition),
     Locality = as.factor(Locality)
   )
 
-# 5. Prepare aggregated community matrix and convert to presence/absence
-comm_agg <- as.matrix(df_agg %>% select(-Locality, -Trees, -Altitude, -Altitude_scaled))
+# 4. Prepare Community Matrix
+# Drop metadata columns to leave only the species columns
+comm_agg <- df_agg %>% select(-Locality, -Elevation, -Elevation_scaled, -Exposition)
+comm_agg <- as.matrix(comm_agg)
 rownames(comm_agg) <- df_agg$Locality
+
+# Convert to presence/absence (binary)
 comm_pa_agg <- ifelse(comm_agg > 0, 1, 0)
 
-# 6. Calculate independent beta-diversity components
+# 5. Calculate independent beta-diversity components using designdist
 dist_jaccard <- designdist(comm_pa_agg, method = "1 - (J / (A + B - J))", terms = "binary")
 dist_simpson <- designdist(comm_pa_agg, method = "1 - (J / pmin(A, B))", terms = "binary")
 dist_richness <- designdist(comm_pa_agg, method = "1 - (pmin(A, B) / pmax(A, B))", terms = "binary")
@@ -53,35 +43,42 @@ dist_jaccard_sqrt  <- sqrt(dist_jaccard)
 dist_simpson_sqrt  <- sqrt(dist_simpson)
 dist_richness_sqrt <- sqrt(dist_richness)
 
-# 7. PERMDISP: testing variance
-disp_jaccard <- betadisper(dist_jaccard_sqrt, df_agg$Trees)
-disp_simpson <- betadisper(dist_simpson_sqrt, df_agg$Trees)
+# 6. PERMDISP: Testing multivariate dispersion (variance)
+# Using Exposition as the categorical grouping factor
+disp_jaccard <- betadisper(dist_jaccard_sqrt, df_agg$Exposition)
+disp_simpson <- betadisper(dist_simpson_sqrt, df_agg$Exposition)
 
 print("--- PERMDISP Results ---")
 print(permutest(disp_jaccard, permutations = 999))
 print(permutest(disp_simpson, permutations = 999))
 
-# 8. PERMANOVA
-# Nestedness
-perm_jaccard <- adonis2(dist_jaccard_agg_sqrt ~ Trees + Altitude_scaled, 
-                               data = df_agg, 
-                               permutations = 999,
-                               by = "margin")
+# 7. PERMANOVA
+# Total beta-diversity (Jaccard)
+perm_jaccard <- adonis2(dist_jaccard_sqrt ~ Exposition + Elevation_scaled, 
+                        data = df_agg, 
+                        permutations = 999,
+                        by = "margin")
 
-# Turnover
-perm_simpson <- adonis2(dist_simpson_agg_sqrt ~ Trees + Altitude_scaled, 
-                               data = df_agg, 
-                               permutations = 999,
-                               by = "margin")
-# Total beta-dievrsity
-perm_richness <- adonis2(dist_richness_agg_sqrt ~ Trees + Altitude_scaled, 
-                                data = df_agg, 
-                                permutations = 999,
-                                by = "margin")
+# Turnover (Simpson)
+perm_simpson <- adonis2(dist_simpson_sqrt ~ Exposition + Elevation_scaled, 
+                        data = df_agg, 
+                        permutations = 999,
+                        by = "margin")
 
-print("--- PERMANOVA of Elevational gradient and Trees ---")
+# Richness difference (Nestedness-resultant)
+perm_richness <- adonis2(dist_richness_sqrt ~ Exposition + Elevation_scaled, 
+                         data = df_agg, 
+                         permutations = 999,
+                         by = "margin")
+
+print("--- PERMANOVA of Elevational gradient and Exposition ---")
+print("Total Beta-Diversity (Jaccard):")
 print(perm_jaccard)
+
+print("Turnover (Simpson):")
 print(perm_simpson)
+
+print("Richness Differences:")
 print(perm_richness)
 
 cor.test(df_agg$Altitude, rowSums(comm_pa_agg))
