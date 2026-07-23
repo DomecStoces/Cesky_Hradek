538aac78f86a491db6df9c14ff98f3165a1f4871
DomecStoces <dominik.stoces@mendelu.cz>
2026-07-23 12:35:59 +0200

    making changes to overall model fit FDRaoQ, CWMs

--- FD_RaoQ.R
+++ FD_RaoQ.R
@@ -5,31 +5,13 @@ library(tibble)
 library(cluster)
 df <- read_excel("df.xlsx")
 
-sp_data <- read_excel("Rao_diversity.xlsx", sheet = "sp")
-traits_data <- read_excel("Rao_diversity.xlsx", sheet = "traits")
-traits_matrix <- column_to_rownames(traits_data, var = "Species")
-sp_matrix <- column_to_rownames(sp_data, var = "ID")
-
-# Strict ordinal scaling
-traits_matrix$Dispersal <- factor(traits_matrix$Dispersal, levels = c(1,2,3), ordered = TRUE)
-traits_matrix$Trophic <- factor(traits_matrix$Trophic, levels = c(1,2,3), ordered = TRUE)
-# Calculate Gower distance for traits
-trait_distance <- daisy(traits_matrix, metric = "gower")
-
-traits_mat <- as.matrix(trait_distance)
-sp_mat <- as.matrix(sp_matrix)
-
-rao_results <- fd_raoq(traits = traits_mat, sp = sp_mat)
-print(rao_results)
-
 ### FD RaoQ calculation in GAM ###
 library(mgcv)
-df <- read_excel("Rao_diversity.xlsx", sheet = "RaoQ")
-df$Altitude_scaled <- as.numeric(scale(df$Elevation, center = TRUE, scale = TRUE))
-df$Locality <- as.factor(df$Locality)
-df$Year <- as.factor(df$Year)
-df$Exposition2 <- as.numeric(df$Exposition2)
-df$Exposition2 <- scale(df$Exposition2)
+df$Elevation_Group  <- as.factor(df$Elevation_Group)
+df$Exposition_Group <- as.factor(df$Exposition_Group)
+df$Locality         <- as.factor(df$Locality)
+df$Year             <- as.factor(df$Year)
+df$Month            <- as.factor(df$Month)
 
 mod_gam_rao <- gam(
   Q ~ 
@@ -40,6 +22,31 @@ mod_gam_rao <- gam(
   family = tw(link="log"), select = TRUE,
   method = "REML"
 )
+
+mod_gam_rao <- gam(
+  Wings_cwm ~ 
+    Elevation_Group +          
+    Exposition_Group +         
+    s(Locality, bs = "re") + s(Month, bs = "re") +
+    s(Year, bs = "re"),        
+  data   = df,
+  family = betar(link="cloglog"), 
+  select = TRUE,
+  method = "REML"
+)
+
+mod_gam_rao <- gam(
+  Body_size_cwm ~ 
+    Elevation_Group +          
+    Exposition_Group +         
+    s(Locality, bs = "re") + s(Month, bs = "re") +
+    s(Year, bs = "re"),        
+  data   = df,
+  family = gaussian(link="identity"), 
+  select = TRUE,
+  method = "REML"
+)
+
 summary(mod_gam_rao)
 par(mfrow = c(2, 2))
 gam.check(mod_gam_rao)
